172ebaaea3fbf42c71d5d3c91308fd694d266e7c
Ilir Bekteshi <ilirsb@gmail.com>
2020-07-25 22:11:59 +0200

    init notifications

---
+++ notifications/facebook.py
@@ -0,0 +1,7 @@
+'''
+TO BE IMPLEMENTED
+
+Send notifications to Facebook Messages
+
+For example if door is left open for a period of time
+'''
---
+++ notifications/viber.py
@@ -0,0 +1,7 @@
+'''
+TO BE IMPLEMENTED
+
+Send notifications to Viber
+
+For example if door is left open for a period of time
+'''
